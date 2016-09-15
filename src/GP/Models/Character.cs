ecbe04c9f52ad2ae53ffaa931c7308a5a1667ab2
craig.farrell <craig.farrell@CANL-HXH1K12>
2016-09-15 10:39:33 -0300

    Add project files.

---
+++ src/GP/Models/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GP.Models
+{
+    public class Character
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CivilizationID { get; set; }
+        public virtual Civilization Civilization { get; set; }
+        public int Health { get; set; }
+        public bool Alive { get; set; }
+        public int Credits { get; set; }
+        public int MilitaryRank { get; set; }
+    }
+}
---
+++ src/GP/Models/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GP.Models
+{
+    public class Planet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CivilizationID { get; set; }
+        public virtual Civilization Civilization { get; set; }
+    }
+}
